ba65f0bdc76807cb525fcd1595a9c013d4a55d0c
suraj.ojha <suraj.ojha@LIS-LAP-100>
2023-05-29 15:09:31 +0545

    added the output files and fixed the not generating problem in case of unrecognized words

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ tokenizer.fit_on_texts(texts=headlineData)
 # print(tokenizer.word_index)
 loaded_model = tf.keras.saving.load_model("nepali_news_headline_model.hdf5")
 
-
 def get_padded_sequence(seed_text):
   # cleaning the sentence
   sentence = seed_text.translate(str.maketrans('', '', string.punctuation))
@@ -30,9 +29,13 @@ def generate_headline(seed_text, model):
   while len(seed_text.split(' ')) < 10:
     textPaddedSequence = get_padded_sequence(seed_text)
     predictedIndex = np.argmax(model.predict([textPaddedSequence], verbose=0))
-    for word, index in tokenizer.word_index.items():
-      if index == predictedIndex:
-        seed_text += " " + word
+    if predictedIndex > len(tokenizer.word_index):
+       seed_text += ' विषय'
+    else: 
+      for word, index in tokenizer.word_index.items():
+        if index == predictedIndex:
+          seed_text += " " + word
+          break
 
   return seed_text
 
@@ -40,7 +43,7 @@ def generate_headline(seed_text, model):
 # Create a text input field
 st.title("Nepali Text Generator")
 user_input = st.text_input("Please provide some context as given in the examples below: ")
-st.text("Examples: बच्चा जन्मिएको एक, दलित र सीमान्त, राज्यविरुद्धको अपराधसहित")
+st.text("Examples: काम नगर्ने ठेकेदारलाई, सर्पदंशका बिरामीलाई, दलित र सीमान्त")
 if st.button("Generate"):
   generated = generate_headline(user_input, loaded_model)
   st.subheader("Generated Headline: " + generated)
